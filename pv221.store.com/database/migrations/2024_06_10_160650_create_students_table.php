307cf7a37d1f78f61b361a439ecc381c6666f2c8
novakvova <novakvova@gmail.com>
2024-06-10 19:18:42 +0300

    Begin working students

---
+++ pv221.store.com/database/migrations/2024_06_10_160650_create_students_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 255);
+            $table->string("email", 255);
+            $table->string("phone", 255);
+            $table->string("address", 512);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
