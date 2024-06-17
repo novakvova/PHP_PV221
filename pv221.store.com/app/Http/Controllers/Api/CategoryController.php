3d4964f43df7193948d9b35239a6fb1aca17429a
novakvova <novakvova@gmail.com>
2024-06-17 23:11:27 +0300

    Upload image is good

--- pv221.store.com/app/Http/Controllers/Api/CategoryController.php
+++ pv221.store.com/app/Http/Controllers/Api/CategoryController.php
@@ -5,9 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class CategoryController extends Controller
 {
+    protected string $upload;
+    protected string $url;
+    public function __construct()
+    {
+        $protocol = isset($_SERVER['HTTPS']) &&
+        $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://';
+        $base_url = $protocol . $_SERVER['HTTP_HOST'] . '/';
+        $this-> upload = env('UPLOAD_DIR');
+        $this-> url = $base_url . $this->upload;
+    }
     public function getList() {
         $data = Category::all();
         return response()->json($data)
@@ -15,10 +27,29 @@ class CategoryController extends Controller
     }
 
     public function create(Request $request) {
-        $inputs = $request->all();
-
-        return response()->json($inputs,201,
-            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'], JSON_UNESCAPED_UNICODE);
+        if (!file_exists(public_path($this->upload))) {
+            mkdir(public_path($this->upload), 0777);
+        }
+        if ($request->hasFile('image')) {
+            $file = $request->file('image');
+            $fileName = uniqid() . '.webp';
+            $destinationPath =public_path( $this->upload);
+            $manager = new ImageManager(new Driver());
+            $sizes = [50,150,300,600,1200];
+            foreach ($sizes as $size) {
+                // read image from file system
+                $imageSave = $manager->read($file);
+                // resize image proportionally to 600px width
+                $imageSave->scale(width: $size);
+                $path = public_path($this->upload.$size."_".$fileName);
+                // save modified image in new format
+                $imageSave->toWebp()->save($path);
+            }
+            $item = Category::create(['name' => $request->input('name') , 'image' => $fileName]);
+            $item->image =$this->url . $item->image;
+            return response()->json($item, 201);
+        }
+        else
+            return response()->json("Image file not found", 404);
     }
-
 }
