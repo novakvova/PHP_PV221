9148150f4055699745b92efb21f3a5cff7e0af0c
novakvova <novakvova@gmail.com>
2024-06-28 19:33:47 +0300

    Add Seeder DB

---
+++ WebTuchyn/WebTuchyn/Data/SeederDB.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+
+namespace WebTuchyn.Data;
+
+public static class SeederDB
+{
+    public static void SeedData(this IApplicationBuilder app)
+    {
+        using (var scope = app.ApplicationServices
+            .GetRequiredService<IServiceScopeFactory>().CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<TuchynDbConext>();
+
+            if (!context.Dogs.Any())
+            {
+                var facker = new Faker<DogEntity>("uk")
+                    .RuleFor(x => x.Name, f => f.Name.FullName())
+                    .RuleFor(x => x.Image, f => f.Image.PicsumUrl());
+                var items = facker.Generate(10);
+                context.Dogs.AddRange(items);
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- WebTuchyn/WebTuchyn/Program.cs
+++ WebTuchyn/WebTuchyn/Program.cs
@@ -26,4 +26,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.SeedData();
+
 app.Run();
